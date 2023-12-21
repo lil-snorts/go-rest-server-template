18a03f8cad66eafec0b08e3090069ef88b969400
gnb <itsgarnett@gmail.com>
2023-12-21 21:30:16 +1300

    POST endpoint created, http client created

--- controller/go.mod
+++ controller/go.mod
@@ -1,1 +1,5 @@
 module controller/mod
+
+go 1.21.5
+
+require github.com/gorilla/mux v1.8.1
--- controller/main.go
+++ controller/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+type Article struct {
+	Id      string `json:"Id"`
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the home page :] ")
@@ -12,10 +28,50 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/articles/{id}", returnOneArticle)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// return the string response containing the request body
+	reqBody, _ := io.ReadAll(r.Body)
+	fmt.Fprintf(w, "%+v", string(reqBody))
+}
+
+func returnOneArticle(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnOneArticle")
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for _, article := range Articles {
+		if article.Id == id {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+
 }
 
 func main() {
+
+	fmt.Println("Restful API server starting on port 10000")
+
+	Articles = []Article{
+		{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+
 	handleRequests()
 }
