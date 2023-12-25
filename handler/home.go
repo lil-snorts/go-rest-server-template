87f4286b6650c94507d05899d44d63e47e7d5094
gnb <itsgarnett@gmail.com>
2023-12-25 22:47:22 +1300

    added user struct and boilerplate for accessing variables in the HTML

--- handler/home.go
+++ handler/home.go
@@ -1,7 +1,8 @@
 package handler
 
 import (
-	pages "github.com/GarNepNep/MaxTrackr-go-app/view"
+	"github.com/GarNepNep/MaxTrackr-go-app/model"
+	"github.com/GarNepNep/MaxTrackr-go-app/view/page"
 	"github.com/labstack/echo/v4"
 )
 
@@ -9,5 +10,7 @@ type HomeHandler struct {
 }
 
 func (hh HomeHandler) HandleHomePage(c echo.Context) error {
-	return pages.Show().Render(c.Request().Context(), c.Response())
+	usr := model.User{
+		Name: "yipee@yahoo.com"}
+	return page.Show(usr).Render(c.Request().Context(), c.Response())
 }
